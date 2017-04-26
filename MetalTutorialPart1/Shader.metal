b7e2fe8ec79f0a1070cadb2367415e144fdbff50
timsheu <timsheu.ash@gmail.com>
2017-04-26 18:26:13 +0800

    commit for latter use

--- MetalTutorialPart1/Shader.metal
+++ MetalTutorialPart1/Shader.metal
@@ -8,6 +8,13 @@
 
 #include <metal_stdlib>
 using namespace metal;
+//TODO: 176*144 is the width/height for testing
+#define IMAGE_WIDTH 176
+#define IMAGE_HEIGHT 144
+
+struct Uniforms{
+    float array[IMAGE_WIDTH*IMAGE_HEIGHT*6];
+};
 
 vertex float4 basic_vertex(const device packed_float3* vertex_array [[ buffer(0)]],
                            unsigned int vid [[ vertex_id ]]) {
@@ -16,4 +23,26 @@ vertex float4 basic_vertex(const device packed_float3* vertex_array [[ buffer(0)
 
 fragment half4 basic_fragment(){
     return half4(1.0);
+}
+
+kernel void YCbCrColoerConversion(texture2d<uint, access:: read> input [[texture(0)]],
+                                  texture2d<uint, access:: write> output [[texture(1)]],
+                                  uint2 gid [[thread_position_in_grid]]){
+    
+}
+
+kernel void YCbCrColoerConversion(texture2d<float, access:: read> yTexture [[texture(0)]],
+                                  texture2d<float, access:: read> cbcrTexture [[texture(1)]],
+                                  texture2d<float, access:: write> outTexture [[texture(2)]],
+                                  uint2 gid [[thread_position_in_grid]]){
+    float3 colorOffset = float3( -(16.0/255.0), -0.5, -0.5);
+    float3x3 colorMatrix = float3x3(float3(1.164,  1.164, 1.164),
+                                    float3(0.000, -0.392, 2.017),
+                                    float3(1.596, -0.813, 0.000));
+    uint2 cbcrCoordinates = uint2(gid.x/2, gid.y/2);
+    float y = yTexture.read(gid).r;
+    float2 cbcr = cbcrTexture.read(cbcrCoordinates).rg;
+    float3 ycbcr = float3(y, cbcr);
+    float3 rgb = colorMatrix * (ycbcr + colorOffset);
+    outTexture.write(float4(float3(rgb), 1.0), gid);
 }
--- MetalTutorialPart1/ViewController.swift
+++ MetalTutorialPart1/ViewController.swift
@@ -12,44 +12,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var showImage: UIView!
     var device: MTLDevice!
     var metalLayer: CAMetalLayer!
-    var vertexData: [Float] = [0.0,  1.0, 0.0,
-                               1.0,  0.0, 0.0,
-                               0.0, -1.0, 0.0,
-                              -1.0,  0.0, 0.0,
-                               0.0,  1.0, 0.0]
-    var vertexBuffer: MTLBuffer!
-    var pipelineState: MTLRenderPipelineState!
-    var commandQueue: MTLCommandQueue!
-    var timer: CADisplayLink!
-    var value = 0.0
+    var queue: MTLCommandQueue!
+    var uniform_buffer: MTLBuffer!
+    var cps: MTLComputePipelineState!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        device = MTLCreateSystemDefaultDevice()
-        
-        metalLayer = CAMetalLayer()
-        metalLayer.device = device
-        metalLayer.pixelFormat = .bgra8Unorm
-        metalLayer.framebufferOnly = true
-        metalLayer.frame = showImage.layer.frame
-        view.layer.addSublayer(metalLayer)
-        
-        
-        let defaultLibrary = device.newDefaultLibrary()
-        let fragmentProgram = defaultLibrary?.makeFunction(name: "basic_fragment")
-        let vertexProgram = defaultLibrary?.makeFunction(name: "basic_vertex")
-        
-        let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
-        pipelineStateDescriptor.vertexFunction = vertexProgram
-        pipelineStateDescriptor.fragmentFunction = fragmentProgram
-        pipelineStateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
-        
-        pipelineState = try! device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
-        
-        commandQueue = device.makeCommandQueue()
-        
-        timer = CADisplayLink(target: self, selector: #selector(ViewController.gameloop))
-        timer.add(to: RunLoop.main, forMode: RunLoopMode.defaultRunLoopMode)
+        initCommon()
+//        timer = CADisplayLink(target: self, selector: #selector(ViewController.gameloop))
+//        timer.add(to: RunLoop.main, forMode: RunLoopMode.defaultRunLoopMode)
     }
 
     override func didReceiveMemoryWarning() {
@@ -58,34 +28,61 @@ class ViewController: UIViewController {
     }
 
     func render() -> Void {
-        guard let drawable = metalLayer?.nextDrawable() else { return }
-        let dataSize = vertexData.count * MemoryLayout.size(ofValue: vertexData[0])
-        vertexBuffer = device.makeBuffer(bytes: vertexData, length: dataSize, options: [])
-        
-        let renderPassDescriptor = MTLRenderPassDescriptor()
-        renderPassDescriptor.colorAttachments[0].texture = drawable.texture
-        renderPassDescriptor.colorAttachments[0].loadAction = .clear
-        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 0.0, green: value/255.0, blue: 5.0/255.0, alpha: 1.0)
-        value += 1.0
-        value.formTruncatingRemainder(dividingBy: 255.0)
-//        vertexData[0] = Float(value)
-//        vertexData[3] = -Float(value)
-        let commandBuffer = commandQueue.makeCommandBuffer()
-        
-        let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
-        renderEncoder.setRenderPipelineState(pipelineState)
-        renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, at: 0)
-        renderEncoder.drawPrimitives(type: .lineStrip, vertexStart: 0, vertexCount: 5)
-        renderEncoder.endEncoding()
-        
-        
-        commandBuffer.present(drawable)
-        commandBuffer.commit()
     }
     
     func gameloop() -> Void {
         autoreleasepool {
             self.render()
+        }
+    }
+    
+    func initCommon() -> Void {
+        if device == nil {
+            device = MTLCreateSystemDefaultDevice()
+        }
+        metalLayer = CAMetalLayer()
+        metalLayer.device = device
+        metalLayer.pixelFormat = .bgra8Unorm
+        metalLayer.framebufferOnly = true
+        metalLayer.frame = showImage.layer.frame
+        view.layer.addSublayer(metalLayer)
+        guard let yuvPath = Bundle.main.path(forResource: "tulips_yuyv422_prog_packed_qcif", ofType: "yuv") else { return }
+            //            setDebugText(string: "\(TAG), testYUV, yuvPath: \(yuvPath)")
+        guard let yuvOriginalData = NSData.init(contentsOfFile: yuvPath) else {
+            NSLog("testYUV: get test yuv data failed")
+            return
+        }
+        let originData = yuvOriginalData.bytes
+        //createUniformBuffer
+//        let bytesPerPixel = 4 * MemoryLayout<Float>.size
+        let bytes = 176 * 144 * 6 * 2
+        uniform_buffer = device.makeBuffer(length: bytes, options: [])
+        let bufferPointer = uniform_buffer.contents()
+        NSLog("before copy")
+        memcpy(bufferPointer, originData, bytes)
+        NSLog("after copy")
+//        memset(bufferPointer, 0, bytes * MemoryLayout<Float>.size)
+        // registerComputerShader
+        queue = device.makeCommandQueue()
+        let library = device.newDefaultLibrary()
+        let kernel = library?.makeFunction(name: "YCbCrColoerConversion")
+        try! cps = device.makeComputePipelineState(function: kernel!)
+    }
+    
+    func computeShader() -> Void {
+        if let drawable = metalLayer.nextDrawable(){
+            let commandBuffer = queue.makeCommandBuffer()
+            let commandEncoder = commandBuffer.makeComputeCommandEncoder()
+            commandEncoder.setComputePipelineState(cps)
+            commandEncoder.setTexture(drawable.texture, at: 0)
+            commandEncoder.setBuffer(uniform_buffer, offset: 0, at: 1)
+            let threadGroupCount = MTLSizeMake(8, 8, 1)
+            let threadGroups = MTLSizeMake(drawable.texture.width / threadGroupCount.width, drawable.texture.height, threadGroupCount.height)
+            commandEncoder.dispatchThreadgroups(threadGroups, threadsPerThreadgroup: threadGroupCount)
+            
+            commandEncoder.endEncoding()
+            commandBuffer.present(drawable)
+            commandBuffer.commit()
         }
     }
 }
